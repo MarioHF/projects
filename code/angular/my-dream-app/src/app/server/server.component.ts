1b8f9737236c483f11b378041730bfbf9dce2d92
MarioHF <mariohf94@hotmail.com>
2019-01-21 17:41:40 -0200

    Enhanced the application with css style and new component

---
+++ code/angular/my-dream-app/src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+/**
+ * Decorator
+ */
+@Component({
+    //Selector should be unique
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
+
