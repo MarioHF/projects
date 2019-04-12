a9e8160a5892e1a0f3b4ecc084762b274e033c71
MarioHF <mariohf94@hotmail.com>
2019-04-11 21:06:21 -0300

    Created new models and component view

--- code/angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ code/angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg'),
+    new Recipe('A second test','Second test','https://toriavey.com/images/2015/04/Brisket-6-1-720x960.jpg')
+  ];
 
   constructor() { }
 
