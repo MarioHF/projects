0232f21a4f465f7e3a1b80bf3005c5786cfa6502
MarioHF <mariohf94@hotmail.com>
2019-07-04 21:39:13 -0300

    updating project isolating views

--- code/angular/course-project/src/app/app.component.ts
+++ code/angular/course-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-project';
+
+ loadedFeature = 'recipe'
+ title = 'course-project';
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature
+  }
+
+  
 }
--- code/angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ code/angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.module';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.module';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe('A Test Recipe','This is simply a test','https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg'),
@@ -18,4 +20,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.selectedRecipe.emit(recipe);
+  }
 }
